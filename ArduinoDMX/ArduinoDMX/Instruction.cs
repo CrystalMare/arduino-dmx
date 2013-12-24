f7eb358f3619cdb90541cd75e66e7cd7bab082de
Sven Olderaan <admin@heaven-craft.net>
2013-12-24 23:14:57 +0000

    Initial Commit

---
+++ ArduinoDMX/ArduinoDMX/Instruction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArduinoDMX
+{
+    public enum Instruction : byte
+    {
+        Set = 53,
+        Clear = 43,
+        Stop = 23
+    }
+}
