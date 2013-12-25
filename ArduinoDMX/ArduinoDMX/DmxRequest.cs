404e210cf04f56506a0489839f6aa68845b38e89
Sven Olderaan <admin@heaven-craft.net>
2013-12-25 21:35:54 +0000

    Added Colorwheel and working version of application

---
+++ ArduinoDMX/ArduinoDMX/DmxRequest.cs
@@ -0,0 +1,26 @@
+﻿namespace ArduinoDMX
+{
+    class DmxRequest
+    {
+        /// <summary>
+        /// A container that contains all information needed for a DMX Request
+        /// </summary>
+        /// <param name="channel">The channel that is affected</param>
+        /// <param name="fixture">The value of the channel</param>
+        /// <param name="i">The instruction</param>
+        public DmxRequest(ushort channel, byte fixture, Instruction i)
+        {
+            Channel = channel;
+            Fixture = fixture;
+            Instruction = i;
+        }
+        /// <summary>
+        /// An emtpy contain that can contain all information for a DMX Request
+        /// </summary>
+        public DmxRequest() { }
+
+        public ushort Channel           { get; set; }
+        public byte Fixture             { get; set; }
+        public Instruction Instruction  { get; set; }
+    }
+}
