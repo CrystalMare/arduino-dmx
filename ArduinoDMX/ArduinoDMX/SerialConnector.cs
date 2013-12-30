3f4a4d1a9f57c0bba2caf2cc3730e524aff32c95
Sven Olderaan <admin@heaven-craft.net>
2013-12-30 23:20:21 +0000

    Added features. too lazy to add a full list of changes.

--- ArduinoDMX/ArduinoDMX/SerialConnector.cs
+++ ArduinoDMX/ArduinoDMX/SerialConnector.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.IO.Ports;
-using System.Windows.Forms;
 using System.Threading;
 
 namespace ArduinoDMX
@@ -12,8 +9,13 @@ namespace ArduinoDMX
     {
         private SerialPort _serial;
         private Thread _connection;
+        private Boolean _keepConnection = true;
 
         private LinkedList<DmxRequest> _requests = new LinkedList<DmxRequest>();
+
+        private Dictionary<ushort, byte> _localDmx = new Dictionary<ushort, byte>();
+
+        private byte[] _clearMessage= { (byte)Instruction.Clear, byte.MinValue, byte.MinValue, byte.MinValue, (byte)Instruction.Stop };
 
 
         /// <summary>
@@ -23,8 +25,6 @@ namespace ArduinoDMX
         /// <param name="speed">The speed in baud</param>
         public SerialConnector(string port, int speed)
         {
-            //TODO: Add a way to select port on startup
-
             //Initialize serial port
             _serial = new SerialPort(port, speed, Parity.None, 8, StopBits.One);
             //Add event handler to print an error from arduino
@@ -32,16 +32,55 @@ namespace ArduinoDMX
             _serial.Open();
 
             //Start thread to handle requests and timing.
-            _connection = new Thread(new ThreadStart(SendData));
+            _connection = new Thread(new ThreadStart(HandleRequests));
             _connection.Start();
         }
 
-        private void DmxSend(ushort channel, byte fixture, Instruction instruction) {
-            byte[] chan = BitConverter.GetBytes(channel);
-            byte[] message = { (byte)Instruction.Set, chan[0], chan[1], fixture, (byte)Instruction.Stop };
-            _serial.Write(message, 0, message.Length);
+        /// <summary>
+        /// Updates the local dmx memory
+        /// </summary>
+        /// <param name="channel">The channel that is affected</param>
+        /// <param name="fixture">The fixture of the affected channel</param>
+        private void UpdateBuffer(ushort channel, byte fixture)
+        {
+            if (!_localDmx.ContainsKey(channel)) _localDmx.Add(channel, fixture);
+            else
+            {
+                _localDmx.Remove(channel);
+                _localDmx.Add(channel, fixture);
+            }
         }
 
+        /// <summary>
+        /// Sends a dmx request through the serial port, it will must be ran on a seperate thread.
+        /// </summary>
+        /// <param name="channel">The channel that is affected</param>
+        /// <param name="fixture">The fixture of the affected channel</param>
+        /// <param name="instruction">The instruction for the request, valid instructions are: Set, Clear.</param>
+        private void DmxSend(ushort channel, byte fixture, Instruction instruction) {
+            if (channel > 512) throw new ArgumentOutOfRangeException("channel", channel, "Channel has to be in DMX Spectrum, 1-512");
+            switch (instruction)
+            {
+                case Instruction.Set:
+                    byte[] chan = BitConverter.GetBytes(channel);
+                    byte[] message = { (byte)instruction, chan[0], chan[1], fixture, (byte)Instruction.Stop };
+                    _serial.Write(message, 0, message.Length);
+                    UpdateBuffer(channel, fixture);
+                    break;
+                case Instruction.Stop:
+                    throw new ArgumentException("Invalid instruction", "instruction");
+                case Instruction.Clear:
+                    _serial.Write(_clearMessage, 0, _clearMessage.Length);
+                    _localDmx.Clear();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Adds a request to the Dmx Request Queue
+        /// </summary>
+        /// <param name="channel">The channel that is affected</param>
+        /// <param name="fixture">The fixtured of the affected channel</param>
         public void DmxSet(ushort channel, byte fixture)
         {
             if (channel > 512 || fixture > 255)
@@ -53,20 +92,46 @@ namespace ArduinoDMX
             _requests.AddLast(req);
         }
 
-        private void SendData()
+        /// <summary>
+        /// This method loops and handles requests when they appear
+        /// </summary>
+        private void HandleRequests()
         {
-            while (true)
+            while (_keepConnection)
             {
                 if (_requests.Count <= 0) continue;
-                var req = _requests.First.Value; _requests.RemoveFirst();
+
+                var req = _requests.First.Value;
+
+                if (req.Instruction == Instruction.Clear) _requests.Clear();
+                else _requests.RemoveFirst();
+
                 DmxSend(req.Channel, req.Fixture, req.Instruction);
                 Thread.Sleep(20);
             }
         }
 
+        /// <summary>
+        /// Called when a serial message is received 
+        /// </summary>
+        /// <param name="sender">The origin of the message</param>
+        /// <param name="e"></param>
         private void _serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             Console.WriteLine(_serial.ReadLine());
+        }
+
+        public void Dispose()
+        {
+            _keepConnection = false;
+            while (_connection.IsAlive);
+            _serial.Close();
+            _serial.Dispose();
+        }
+
+        public void ResetFixtures()
+        {
+            _requests.AddFirst(new DmxRequest(1, 1, Instruction.Clear));
         }
     }
 }
--- ArduinoDMX/ArduinoDMX/UserInterface.Designer.cs
+++ ArduinoDMX/ArduinoDMX/UserInterface.Designer.cs
@@ -28,29 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.colorWheel1 = new Unclassified.UI.ColorWheel();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arduinoDMXToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorWheel1 = new Unclassified.UI.ColorWheel();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // colorWheel1
-            // 
-            this.colorWheel1.Hue = ((byte)(0));
-            this.colorWheel1.Lightness = ((byte)(0));
-            this.colorWheel1.Location = new System.Drawing.Point(12, 12);
-            this.colorWheel1.Name = "colorWheel1";
-            this.colorWheel1.Saturation = ((byte)(0));
-            this.colorWheel1.SecondaryHues = null;
-            this.colorWheel1.Size = new System.Drawing.Size(200, 200);
-            this.colorWheel1.TabIndex = 0;
-            this.colorWheel1.Text = "colorWheel1";
-            this.colorWheel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.colorWheel1_MouseUp);
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(219, 12);
+            this.checkBox1.Location = new System.Drawing.Point(408, 119);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(57, 17);
             this.checkBox1.TabIndex = 1;
@@ -59,23 +54,99 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(219, 36);
+            this.numericUpDown1.Location = new System.Drawing.Point(408, 142);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(57, 20);
             this.numericUpDown1.TabIndex = 2;
             this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arduinoDMXToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(897, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arduinoDMXToolStripMenuItem
+            // 
+            this.arduinoDMXToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.arduinoDMXToolStripMenuItem.Name = "arduinoDMXToolStripMenuItem";
+            this.arduinoDMXToolStripMenuItem.Size = new System.Drawing.Size(91, 20);
+            this.arduinoDMXToolStripMenuItem.Text = "Arduino DMX";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 469);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(897, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(509, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // colorWheel1
+            // 
+            this.colorWheel1.Hue = ((byte)(0));
+            this.colorWheel1.Lightness = ((byte)(0));
+            this.colorWheel1.Location = new System.Drawing.Point(85, 92);
+            this.colorWheel1.Name = "colorWheel1";
+            this.colorWheel1.Saturation = ((byte)(0));
+            this.colorWheel1.SecondaryHues = null;
+            this.colorWheel1.Size = new System.Drawing.Size(200, 200);
+            this.colorWheel1.TabIndex = 0;
+            this.colorWheel1.Text = "colorWheel1";
+            this.colorWheel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.colorWheel1_MouseUp);
+            // 
             // UserInterface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(478, 227);
+            this.ClientSize = new System.Drawing.Size(897, 491);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.colorWheel1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
             this.Name = "UserInterface";
             this.Text = "UserInterface";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.UserInterface_FormClosed);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -86,6 +157,12 @@
         private Unclassified.UI.ColorWheel colorWheel1;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arduinoDMXToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button button1;
 
     }
 }
